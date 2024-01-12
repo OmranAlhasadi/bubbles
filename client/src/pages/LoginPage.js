38d49b0ab0702a358d1a9febc36e821adeacee77
Omran <omranelhasadi@gmail.com>
2024-01-12 12:31:25 +0800

    Added signup/login html

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,20 +1,104 @@
 import styles from "../css/LoginPage.module.css";
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 const LoginPage = () => {
   const { loginUser } = useContext(UserContext);
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const handleLogin = () => {
-    loginUser(username, password);
-    // Redirect user to homepage or show error
+  const navigateToSignUp = () => {
+    navigate("/signup");
+  };
+
+  const navigateToForgotPassword = () => {
+    navigate("/forgot-password");
+  };
+
+  const validateFields = () => {
+    let isValid = true;
+
+    //empty fields validation
+
+    if (!username.trim()) {
+      setUsernameError("Username cannot be empty.");
+      isValid = false;
+    } else {
+      setUsernameError("");
+    }
+
+    if (!password.trim()) {
+      setPasswordError("Password cannot be empty.");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    return isValid;
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (validateFields()) {
+      await loginUser(username, password);
+      navigate("/");
+    }
   };
 
   return (
-    <div>
-      <div></div>
+    <div className={styles.Container}>
+      <div className={styles.formContainer}>
+        <form className={styles.form} onSubmit={handleLogin}>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="username">
+              Username
+            </label>
+            <input
+              className={styles.inputField}
+              name="username"
+              placeholder="Enter username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameError && (
+              <div className={styles.errorMsg}>{usernameError}</div>
+            )}
+          </div>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="password">
+              Password
+            </label>
+            <input
+              className={styles.inputField}
+              name="password"
+              placeholder="Enter password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordError && (
+              <div className={styles.errorMsg}>{passwordError}</div>
+            )}
+          </div>
+          <button className={styles.formButton} type="submit">
+            Log in
+          </button>
+          <button type="button" className={styles.forgotButton}>
+            forgot your password?
+          </button>
+          <button type="button" className={styles.formButton}>
+            Log in as Example User
+          </button>
+          <button type="button" className={styles.formButton}>
+            Sign Up with Email
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,9 +1,188 @@
 import styles from "../css/SignupPage.module.css";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
 
 const SignupPage = () => {
+  const { signupUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [name, setanaame] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [eail, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  const validateFields = () => {
+    let isValid = true;
+
+    //empty fields validation
+
+    if (!name.trim()) {
+      setNameError("Name cannot be empty.");
+      isValid = false;
+    } else {
+      setNameError("");
+    }
+
+    if (!username.trim()) {
+      setUsernameError("Username cannot be empty.");
+      isValid = false;
+    } else {
+      setUsernameError("");
+    }
+
+    if (!email.trim()) {
+      setEmailError("Email cannot be empty.");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (!password.trim()) {
+      setPasswordError("Password cannot be empty.");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (!confirmPassword.trim()) {
+      setConfirmPasswordError("Confirm Password cannot be empty.");
+      isValid = false;
+    } else {
+      setConfirmPasswordError("");
+    }
+
+    //format validation
+
+    // password match validation
+    if (password !== confirmPassword) {
+      setConfirmPasswordError("Confirm Password must match Password.");
+      isValid = false;
+    } else {
+      setConfirmPasswordError("");
+    }
+
+    // Email format validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.trim())) {
+      setEmailError("Invalid email format.");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+
+    // Password length validation
+    if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long.");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    return isValid;
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    if (validateFields()) {
+      await signupUser(username, password);
+      navigate("/login");
+    }
+  };
+
   return (
-    <div>
-      <div></div>
+    <div className={styles.Container}>
+      <div className={styles.formContainer}>
+        <form className={styles.form} onSubmit={handleLogin}>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="name">
+              Full name
+            </label>
+            <input
+              className={styles.inputField}
+              name="name"
+              placeholder="Enter full name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {nameError && <div className={styles.errorMsg}>{nameError}</div>}
+          </div>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="username">
+              Username
+            </label>
+            <input
+              className={styles.inputField}
+              name="username"
+              placeholder="Enter username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameError && (
+              <div className={styles.errorMsg}>{usernameError}</div>
+            )}
+          </div>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="email">
+              Email
+            </label>
+            <input
+              className={styles.inputField}
+              name="email"
+              placeholder="Enter email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailError && <div className={styles.errorMsg}>{emailError}</div>}
+          </div>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="password">
+              Password
+            </label>
+            <input
+              className={styles.inputField}
+              name="password"
+              placeholder="Enter password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordError && (
+              <div className={styles.errorMsg}>{passwordError}</div>
+            )}
+          </div>
+          <div className={fieldContainer}>
+            <label className={fieldLabel} htmlFor="confirmPassword">
+              Confirm password
+            </label>
+            <input
+              className={styles.inputField}
+              name="confirmPassword"
+              placeholder="Re-enter password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {confirmPasswordError && (
+              <div className={styles.errorMsg}>{confirmPasswordError}</div>
+            )}
+          </div>
+          <button className={styles.formButton} type="submit">
+            Sign up
+          </button>
+          <button type="button" className={styles.loginButton}>
+            Already a user? Login!
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
