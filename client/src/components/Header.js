db1f1d56ac638bdf7a1936fcb5b22a02e781f123
Omran <omranelhasadi@gmail.com>
2024-05-30 11:43:15 +0800

    fixed toggleRequests button double-toggle problem

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,10 +13,11 @@ const Header = () => {
 
   const [showRequests, setShowRequests] = useState(false);
   const requestsRef = useRef();
+  const svgRef = useRef();
 
   // toggle friend request list
   const toggleRequests = () => {
-    setShowRequests(!showRequests);
+    setShowRequests((showRequests) => !showRequests);
   };
 
   const handleProfileClick = () => {
@@ -45,7 +46,12 @@ const Header = () => {
   useEffect(() => {
     //check if click is outside the ref element
     function handleClickOutside(event) {
-      if (requestsRef.current && !requestsRef.current.contains(event.target)) {
+      if (
+        requestsRef.current &&
+        !requestsRef.current.contains(event.target) &&
+        svgRef.current &&
+        !svgRef.current.contains(event.target)
+      ) {
         setShowRequests(false);
       }
     }
@@ -144,6 +150,7 @@ const Header = () => {
           id={styles.requests}
           className={styles.svg}
           onClick={toggleRequests}
+          ref={svgRef}
           viewBox="0 0 16 16"
         >
           <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
